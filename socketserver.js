73d732bbf0cb8b7aa34418ee081341183f5fd4c0
Danara Sarioglu <ds4489@nyu.edu>
2015-10-10 16:28:42 -0400

    fix

--- socketserver.js
+++ socketserver.js
@@ -3,22 +3,30 @@ var http = require('http');
 var fs = require('fs'); // Using the filesystem module
 var httpServer = http.createServer(requestHandler);
 httpServer.listen(8080);
+var url =  require('url');
 
 function requestHandler(req, res) {
-	// Read index.html
-	fs.readFile(__dirname + '/index.html', 
-		// Callback function for reading
-		function (err, data) {
-			// if there is an error
-			if (err) {
-				res.writeHead(500);
-				return res.end('Error loading index.html');
-			}
-			// Otherwise, send the data, the contents of the file
-			res.writeHead(200);
-			res.end(data);
-  		}
-  	);
+   var parsedUrl = url.parse(req.url);
+    console.log("They asked for " + parsedUrl.pathname);
+
+    var path = parsedUrl.pathname;
+    if (path == "/") {
+            path = "index.html";
+    }
+
+    fs.readFile(__dirname + path,
+        // Callback function for reading
+        function (err, data) {
+            // if there is an error
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            // Otherwise, send the data, the contents of the file
+            res.writeHead(200);
+            res.end(data);
+          }
+    );
 }
 
 // WebSocket Portion
